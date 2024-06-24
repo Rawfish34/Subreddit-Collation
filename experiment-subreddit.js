184a97fddda99b18207ac866c3521332e4f884d1
Rawfish34 <167566616+Rawfish34@users.noreply.github.com>
2024-06-25 02:37:11 +0800

    Update 5

--- experiment-subreddit.js
+++ experiment-subreddit.js
@@ -8,6 +8,9 @@ const subsFromLocalStorage=JSON.parse(localStorage.getItem("subreddits"));
 const sub_length_input=document.getElementById("sub-length");
 const sub_length_button=document.getElementById("sub-length-btn");
 var subLength=12;
+var isSorted=false;
+const likes=document.getElementById("likes")
+
 
 //EVENT LISTENERS
 sub_button.addEventListener("click",addSub)
@@ -23,6 +26,8 @@ sub_length_input.addEventListener("keydown",(e)=>{
         changeLength();
     }
 })
+
+likes.addEventListener("click",sort_subs)
 
 //LOCAL STORAGE
 if (subsFromLocalStorage){
@@ -48,10 +53,25 @@ function changeLength(){
 function process_subs(arr){
     subreddit_list.innerHTML="";
     postsContainer.innerHTML="";
-    arr.forEach((sub)=>{
+    arr.forEach((sub,index)=>{
         fetchSubTopics(sub);
-        subreddit_list.innerHTML+=`<li>${sub}</li>`
+        
+        var li=document.createElement("li");
+        li.innerHTML=`${sub} <button class="delete-btn" data-index="${index}">Delete</button>`
+        subreddit_list.appendChild(li);
+
+        //Delete btn event listener
+        const delete_btn=li.querySelector(".delete-btn");
+        delete_btn.addEventListener("click", ()=>{
+            deleteIndSub(index)
+        })
     })
+}
+
+function deleteIndSub(index){
+    subreddit_arr.splice(index, 1);
+    localStorage.setItem("subreddits", JSON.stringify(subreddit_arr));
+    process_subs(subreddit_arr);
 }
 
 function addSub(){
@@ -74,21 +94,36 @@ async function fetchSubTopics(subreddit){
 }
 
 function captureData(data){
-    console.log(data)
+    
     const data_children=data.data.children;
     const children_arr_sliced=data_children.slice(0,`${subLength}`)
+    console.log(children_arr_sliced);
+    if (isSorted){
+        children_arr_sliced.sort((a,b)=>{
+            if (a.data.ups<b.data.ups){
+                return 1;
+            }
+            else if (a.data.ups>b.data.ups){
+                return -1;
+            }
+        })
+        
+    }
+    
     process_data(children_arr_sliced)
 }
 
 function process_data(obj){
     var subreddit_name=obj[0].data.subreddit;
-    postsContainer.innerHTML+=`<div class="subreddit-title" style="font-size: 1.5rem; background-color: var(--medium-grey);">${subreddit_name}</div>`
+    postsContainer.innerHTML+=`<div class="subreddit-title" style="font-size: 1.5rem; background-color: var(--medium-grey); font-family: 'Montserrat', sans-serif;">${subreddit_name}</div>`
     let html='';
     html=obj.map((post,index)=>{
         var info=post.data;
        
-        const {title, ups, upvote_ratio, num_comments,permalink,subreddit}=info;
+        const {title, ups, upvote_ratio, num_comments,permalink, created_utc, subreddit}=info;
 
+        //GET TIME SINCE CREATED
+        var timeSince=time_calc(created_utc)
         
 
         // Define thicker bottom border for the last row
@@ -101,8 +136,9 @@ function process_data(obj){
         <a class="post-title" href="https://www.reddit.com/${permalink}" target="_blank">${title}</a>
         </td>
        <td>${ups}</td>
-       <td>${upvote_ratio}</td>
-       <td class="last-td">${num_comments}</td>
+       <td>${timeSince}</td> 
+       <td>${num_comments}</td>
+       <td class="last-td">${upvote_ratio}</td>
         </tr>
         
         
@@ -111,5 +147,31 @@ function process_data(obj){
     postsContainer.innerHTML+=html;
 }
 
+//SORT AND OTHER FLAVOR FUNCTIONS
+function sort_subs(){
+    isSorted=!isSorted;
+    process_subs(subreddit_arr);
+}
 
+//TIME created since function
+function time_calc(createdTime){
+    const currentTime=Math.floor(Date.now()/1000)//in seconds
+    const time_dif_seconds=currentTime-createdTime
+    const minutes=Math.floor(time_dif_seconds/60);
+    const hours=Math.floor(minutes/60);
+    const days=Math.floor(hours/24);
+    
 
+    if (time_dif_seconds<60){
+        return `${time_dif_seconds} seconds ago`;
+    }
+    else if (minutes<60){
+        return `${minutes} minutes ago`
+    }
+    else if (hours<24){
+        return `${hours} hours ago`
+    }
+    else {
+        return `${days} days ago`
+    }
+}
